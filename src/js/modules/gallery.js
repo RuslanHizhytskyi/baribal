618247790af81ecdabfe8674252e67d96082f5b4
Rhyzhytskyi <r.hyzhytskyi@everad.com>
2021-02-01 20:48:09 +0200

    fix tel numbers

--- src/js/modules/gallery.js
+++ src/js/modules/gallery.js
@@ -40,7 +40,7 @@ function gallery() {
   }
   function initPhotoSlider() {
     $('.photo__wrapper').slick({
-      
+
       arrows: true,
       prevArrow: "<span class='photo__arrow photo__arrow_prev'><svg viewBox='0 0 34 40' width='34' height='40'><use xlink:href='#arrow_prev'></use></svg></span>",
       nextArrow: "<span class='photo__arrow photo__arrow_next'><svg viewBox='0 0 34 40' width='34' height='40'><use xlink:href='#arrow_next'></use></svg></span>",
@@ -72,65 +72,6 @@ function gallery() {
       closeBigSlider();
     }
   });
-
-
-
-
-
-
-
-
-
-
-  // const gallery = document.querySelector('.gallery__wrapper'),
-  //       bigPhotoWrapper = gallery.querySelector('.photo'),
-  //       bigPhoto = gallery.querySelector('.photo__img'),
-  //       images = gallery.querySelectorAll('.gallery__img'),
-  //       arrowNext = gallery.querySelector('.photo__arrow_next'),
-  //       arrowPrev = gallery.querySelector('.photo__arrow_prev'),
-  //       close = gallery.querySelector('.photo__close');
-
-  // images.forEach((el, index) => {
-  //   el.setAttribute('data-index', index + 1);
-  // });
-
-  // function closePhoto() {
-  //   bigPhoto.setAttribute('src', '');
-  //   bigPhoto.setAttribute('data-index', '');
-  //   bigPhotoWrapper.classList.remove('photo_active');
-  // }
-
-  // gallery.addEventListener('click', (evt) => {
-  //   if (evt.target.classList.contains('gallery__img')) {
-  //     bigPhoto.setAttribute('src', evt.target.getAttribute('src'));
-  //     bigPhoto.setAttribute('data-index', evt.target.getAttribute('data-index'));
-  //     bigPhotoWrapper.classList.add('photo_active');
-  //   } else if (evt.target.classList.contains('photo_active')) {
-  //     closePhoto();
-  //   }
-  // });
-
-  // close.addEventListener('click', closePhoto);
-
-  // function changeSlide(n) {
-  //   let index = +bigPhoto.getAttribute('data-index');
-  //   if ((index + n) < 1) {
-  //     index = images.length;
-  //   } else if ((index + n) > images.length) {
-  //     index = 1;
-  //   } else {
-  //     index += n;
-  //   }
-  //   bigPhoto.setAttribute('data-index', index);
-  //   bigPhoto.setAttribute('src', images[index - 1].getAttribute('src'));
-  // }
-
-  // arrowNext.addEventListener('click', () => {
-  //   changeSlide(1);
-  // });
-  // arrowPrev.addEventListener('click', () => {
-  //   changeSlide(-1);
-  // });
 
 }
 
